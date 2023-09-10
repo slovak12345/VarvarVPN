c5cbc863f842ea8b8186e4c59424b87198d758a6
vicislave <slave@altione.altione.net>
2023-09-10 14:15:48 +0300

    asdweq

--- script/build_docker.py
+++ script/build_docker.py
@@ -6,22 +6,15 @@ try:
 except:
     pass
 
-l = logging.getLogger(__name__)
-
-c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler("logs/build_docker.log")
-
-c_handler.setLevel(logging.INFO)
-f_handler.setLevel(logging.INFO)
-
-format_ = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
-
-c_handler.setFormatter(format_)
-f_handler.setFormatter(format_)
-
-l.addHandler(c_handler)
-l.addHandler(f_handler)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("logs/build_docker.log"),
+        logging.StreamHandler()
+    ]
+)
 
 root_path=os.chdir("../")
 
-l.info(f"Root path of repository: {root_path}")
+logging.info(f"Root path of repository: {root_path}")
