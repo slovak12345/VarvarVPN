b300d1e3427e48b46a5d755a9b15b07cdfddab25
vicislave <slave@altione.altione.net>
2023-09-10 14:09:45 +0300

    asd

--- script/build_docker.py
+++ script/build_docker.py
@@ -1,14 +1,27 @@
 import os
-import logging as l
+import logging
 
 try:
     os.mkdir("logs")
 except:
     pass
 
+l = logging.getLogger(__name__)
 
-l.basicConfig(level=l.INFO, filename="logs/build_docker.log", filemode="w")
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler("logs/build_docker.log")
+
+c_handler.setLevel(logging.INFO)
+f_handler.setLevel(logging.INFO)
+
+format_ = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+
+c_handler.setFormatter(format_)
+f_handler.setFormatter(format_)
+
+l.addHandler(c_handler)
+l.addHandler(f_handler)
 
 root_path=os.chdir("../")
 
-l.info(root_path)
+l.info(f"Root path of repository: {root_path}")
