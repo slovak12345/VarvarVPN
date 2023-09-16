894cf4a3c55bfd741dba924e43bc7b3693646c02
vicislave <slave@altione.altione.net>
2023-09-16 18:21:24 +0300

    asdqe

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
+CONTAINER_TYPE = release
+
+.PHONY: all docker
+
 all: docker
 
 install:
 	echo "install"
 build:
 	echo "build"
-docker:
-	python3 script/build_docker.py
+docker: 
+	python3 script/build_docker.py $(CONTAINER_TYPE)
 clean:
 	git clean -f -x -d *
--- script/build_docker.py
+++ script/build_docker.py
@@ -1,7 +1,8 @@
 import os
 import logging
+import sys
 
-docker_name="VarvarVPN"
+docker_name="varvar_vpn"
 try:
     os.mkdir("logs")
 except:
@@ -16,16 +17,28 @@ logging.basicConfig(
     ]
 )
 
+exp_type=sys.argv[1]
+
+logging.info(f"Selected type: {exp_type}")
+
 root_path=os.path.abspath(".")
 
 logging.info(f"Root path of repository: {root_path}")
+
+docker_compose_path=root_path + "/Docker-compose.yml"
+
+logging.info(f"Docker compose path of repository: {docker_compose_path}")
+
+mount_path=root_path
+
+container_path="/home/varvarvpn"
 
 with open(root_path + "/VERSION") as f:
     VERSION = f.read()
 
 logging.info(f"Start creating docker image of VarvarVPN with version {VERSION}...")
 
-os.system(f"docker build -f {root_path + "/Dockerfile"} -t {docker_name}:{VERSION} .")
+os.system(f"export CONTAINER_PATH=${container_path} && export MOUNT_PATH=${root_path} && docker-compose -f {docker_compose_path} run --rm --name {docker_name}_{exp_type} {docker_name}_{exp_type} bash")
 
 logging.info(f"Finished build docker image {docker_name}:{VERSION}:")
 
